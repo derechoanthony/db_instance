2bb211b33ad83bc598ab1d8de712df5a51cd7c08
derechoanthony <derechoanthony@gmail.com>
2020-02-25 09:12:17 +0800

    Update memberprofile.sql

--- functions/memberprofile.sql
+++ functions/memberprofile.sql
@@ -26,8 +26,8 @@ CREATE OR REPLACE FUNCTION public.memberprofile(
 AS $function$
 
 BEGIN
-    INSERT INTO users_profile(first_name,middle_name,last_name,suffix,nickname,bdate,mobile_no,present_address,permanent_address,email,father_full_name,mother_full_name,Beneficiary_full_name,applicationcode,memberKey)
-    VALUES(_first_name,_middle_name,_last_name,_suffix,_nickname,_bdate,_mobile_no,_present_address,_permanent_address,_email,_father_full_name,_mother_full_name,_Beneficiary_full_name,_regCode,_memberKey);   
+    INSERT INTO users_profile(first_name,middle_name,last_name,suffix,nickname,mobile_no,present_address,permanent_address,email,father_full_name,mother_full_name,Beneficiary_full_name,applicationcode,memberKey)
+    VALUES(_first_name,_middle_name,_last_name,_suffix,_nickname,_mobile_no,_present_address,_permanent_address,_email,_father_full_name,_mother_full_name,_Beneficiary_full_name,_regCode,_memberKey);   
     INSERT INTO users(profile_id,userName,userPassword)values((SELECT id FROM users_profile WHERE email=_email),_email,_password);
     INSERT INTO user_roles(profile_id,roles_id)values((SELECT id FROM users_profile WHERE email=_email),(select id from roles where name='member'));
     UPDATE applicationcodes SET count = count - 1 where key = _regCode;
